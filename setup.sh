f10a60bbe51ddcfd89c85519cc53ce46cab02075
Erik Fastermann <erik@fastermann.de>
2019-02-07 12:17:48 +0100

    setup.sh - Added ZSH plugin install

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Configure the path of the repository
+# NOTE: If ZSH doesn't work properly (bash-completion errors),
+# you maybe have to reconfigure your existing bashrc.
+
+# Path of the dotfiles folder
 dotfiles_dir="${HOME}/dotfiles"
+
 
 # .bashrc
 echo "source ${dotfiles_dir}/.bashrc" >> ~/.bashrc
@@ -12,3 +16,13 @@ echo "source ${dotfiles_dir}/.zshrc" >> ~/.zshrc
 # .vimrc for neovim
 mkdir -p ~/.config/nvim/
 echo "so ${dotfiles_dir}/.vimrc" >> ~/.config/nvim/init.vim
+
+
+# ZSH Autosuggestions
+mkdir -p ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+
+# ZSH Syntax highlighting
+mkdir -p ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
