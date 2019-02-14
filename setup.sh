e03c904d98237eb1e8d7946e62f672217226f5b7
Erik Fastermann <erik@fastermann.de>
2019-02-14 16:25:34 +0100

    Setup: Download Git completion script

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ dotfiles_dir="${HOME}/dotfiles"
 # .bashrc
 echo "source ${dotfiles_dir}/.bashrc" >> ~/.bashrc
 
+# Install Git BASH completion
+sudo curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+-o /etc/bash_completion.d/git-completion.bash
+
 # .vimrc for neovim
 mkdir -p ~/.config/nvim/
 echo "so ${dotfiles_dir}/.vimrc" >> ~/.config/nvim/init.vim
