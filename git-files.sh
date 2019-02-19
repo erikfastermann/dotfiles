872663478744f38b312f4ea401885a0fb3ccc5e2
Erik Fastermann <erik@fastermann.de>
2019-02-19 12:54:14 +0100

    Git-files: Added --follow

--- git-files.sh
+++ git-files.sh
@@ -1,8 +1,8 @@
 while true; do
-    git_file=$(git log --diff-filter=A --pretty=format: --name-only | grep . | fzf)
+    git_file=$(git log --follow --diff-filter=A --pretty=format: --name-only . | grep . | fzf)
     if [[ "$git_file" ]]; then
         while true; do
-            git_commit=$(git log --pretty=format:'%C(auto)%h %Cblue(%an %ar)%Creset %s' -- "$git_file" | fzf | cut -c1-7)
+            git_commit=$(git log --follow --pretty=format:'%C(auto)%h %Cblue(%an %ar)%Creset %s' -- "$git_file" | fzf | cut -c1-7)
             if [[ "$git_commit" ]]; then
                 git show "${git_commit}:${git_file}" | vim
             else
