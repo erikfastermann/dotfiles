3e36f41966776f0f9d0de219f6f4bfbd5256cedb
Erik Fastermann <erik@fastermann.de>
2019-02-19 12:23:48 +0100

    Added easy way to navigate commits for a file

---
+++ git-files.sh
@@ -0,0 +1,15 @@
+while true; do
+    git_file=$(git log --diff-filter=A --pretty=format: --name-only | grep . | fzf)
+    if [[ "$git_file" ]]; then
+        while true; do
+            git_commit=$(git log --pretty=format:'%C(auto)%h %Cblue(%an %ar)%Creset %s' -- "$git_file" | fzf | cut -c1-7)
+            if [[ "$git_commit" ]]; then
+                git show "${git_commit}:${git_file}" | vim
+            else
+                break
+            fi
+        done
+    else
+        break
+    fi
+done
