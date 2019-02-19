46b8f409d13fcee6c4a8e980021659c9ad1d4e00
Erik Fastermann <erik@fastermann.de>
2019-02-19 13:16:07 +0100

    Git-files: Restructured code

--- git-files.sh
+++ git-files.sh
@@ -1,15 +1,7 @@
-while true; do
-    git_file=$(git log --follow --diff-filter=A --pretty=format: --name-only . | grep . | fzf)
-    if [[ "$git_file" ]]; then
-        while true; do
-            git_commit=$(git log --follow --pretty=format:'%C(auto)%h %Cblue(%an %ar)%Creset %s' -- "$git_file" | fzf | cut -c1-7)
-            if [[ "$git_commit" ]]; then
-                git show "${git_commit}:${git_file}" | vim
-            else
-                break
-            fi
-        done
-    else
-        break
-    fi
+while git_file="$(git log --follow --diff-filter=A --pretty=format: --name-only . \
+    | grep . | fzf)"; [[ "$git_file" ]]; do
+    while git_commit="$(git log --follow --pretty=format:'%C(auto)%h %Cblue(%an %ar)%Creset %s' \
+        -- $git_file | fzf | cut -c1-7)"; [[ "$git_commit" ]]; do
+        git show "${git_commit}:${git_file}" | vim
+    done
 done
